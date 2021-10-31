5c24ebd8b3cf86f627cc0a27926922b11b217eee
MaTY <62469448+MaTY-MT@users.noreply.github.com>
2021-10-31 13:40:52 +0100

    change the installation process

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
-import requests
-
+import zipfile
 print("Welcome to the SCR-Autopilot installer for Windows.")
-print("This script will:\n- Clone scr-autopilot/scr-autopilot from GitHub\n- Download and run Tesseract-OCR installer\n- Download and run AutoHotKey installer")
+print("This script will:\n- Download scr-autopilot/scr-autopilot from GitHub\n- Download and run Tesseract-OCR installer\n- Download and run AutoHotKey installer\n- Download all required modules for SCR-Autopilot")
 proceed = input("\nDo you wish to proceed? (Y/n) ")
 if proceed == "n":
     print("canceled")
     exit()
-print("Cloning SCR-Autopilot repository...")
-os.system("git clone https://github.com/scr-autopilot/scr-autopilot.git")
+print("Getting requests module...")
+os.system("py -m pip install requests")
+print("Downloading SCR-Autopilot repository...")
+import requests
+url = "https://github.com/scr-autopilot/scr-autopilot/archive/refs/heads/main.zip"
+r = requests.get(url)
+open("scr-autopilot-main.zip", "wb").write(r.content)
+with zipfile.ZipFile("./scr-autopilot-main.zip", 'r') as zip_ref:
+    zip_ref.extractall("./scr-autopilot")
 print("\nDownloading Tesseract-OCR...")
 url = "https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w32-setup-v5.0.0-alpha.20201127.exe"
 r = requests.get(url)
@@ -28,7 +34,8 @@ input("\nAfter done with AHK setup, press enter.")
 print("Deleting setup files...")
 os.remove("tesseract-ocr-w32-setup-v5.0.0-alpha.20201127.exe")
 os.remove("AutoHotkey_1.1.33.10_setup.exe")
+os.remove("scr-autopilot-main.zip")
 print("Setup files removed.")
 print("Getting all required modules for SCR-Autopilot...")
-os.system("pip3 install opencv_python requests PyDirectInput ahk Flask numpy pytesseract Pillow pywin32")
+os.system("py -m pip install opencv_python requests PyDirectInput ahk Flask numpy pytesseract Pillow pywin32")
 input("\n\nSCR-AUTOPILOT WAS SUCCESSFULLY INSTALLED! Press enter to exit.")
